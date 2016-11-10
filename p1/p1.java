2f219fe65941f9e720b7414eba3a4a6f9cbf62a5
Kevin Wu <kevinwu1106@gmail.com>
2016-11-10 18:25:12 +0100

    actual working r list

--- p1/p1.java
+++ p1/p1.java
@@ -10,6 +10,37 @@ public class p1{
 	private static Vector<Integer> facBase = new Vector<Integer>();
 	private static long L;
 	private static Vector<BigInteger> rList = new Vector<BigInteger>();
+	private static Vector<BigInteger> facRList = new Vector<BigInteger>();
+
+/*
+	public static void _generateRList(){
+		int count = 0;
+		long sqrtL = (long)Math.sqrt(L)+1;
+		sqrtL = 2000;
+		for (BigInteger j = BigInteger.valueOf(1); j.compareTo(BigInteger.valueOf(sqrtL)) <= 0; j = j.add(BigInteger.ONE)){
+			for (BigInteger k = BigInteger.valueOf(1); k.compareTo(BigInteger.valueOf(sqrtL)) <= 0; k = k.add(BigInteger.ONE)){
+				rList.add(generateR(k, j));	
+			//	if (++count >= L)return;
+			}
+		}
+	}
+
+	public static void cleanRList(){
+		for (int i = 0; i < rList.size(); i++){
+			BigInteger y = (rList.get(i).multiply(rList.get(i))).mod(N);
+			for (int j = 0; j < facBase.size(); j++){
+				while (y.mod(BigInteger.valueOf(facBase.get(j))).compareTo(BigInteger.ZERO) == 0){
+					y = y.divide(BigInteger.valueOf(facBase.get(j)));
+						
+					if (y.compareTo(BigInteger.ONE) ==0)
+						facRList.add(rList.get(i));
+				}
+
+			}
+		}
+
+	}
+*/
 
 	public static boolean isPrime(int num){
 		if (num<2) return false;
@@ -44,20 +75,45 @@ public class p1{
 	}
 
 	public static void generateRList(){
-		int count = 0;
-		long sqrtL = (long)Math.sqrt(L)+1;
-		for (BigInteger j = BigInteger.valueOf(1); j.compareTo(BigInteger.valueOf(sqrtL)) <= 0; j = j.add(BigInteger.ONE)){
-			for (BigInteger k = BigInteger.valueOf(1); k.compareTo(BigInteger.valueOf(sqrtL)) <= 0; k = k.add(BigInteger.ONE)){
-				rList.add(generateR(k, j));	
-				if (++count >= L)return;
+		int max = 1;
+		int k = max;
+		int j = max;
+		do {
+			BigInteger x = generateR(BigInteger.valueOf(k), BigInteger.valueOf(j)); 
+			addFactor(x, (x.multiply(x)).mod(N));
+			//incrementing max
+			if (j == 1 && k == max){
+				max++; j = max; k = 1; continue;
 			}
+			//incrementing k
+			if (j == max && k < max){
+				k++; continue;
+			}
+			//cecrement j
+			if (j > 1 && k == max){
+				j--; continue;
+			}
+		} while (rList.size() < L);
+	}
+
+	public static void addFactor(BigInteger x, BigInteger y){
+		for (int j = 0; j < facBase.size(); j++){
+			while (y.mod(BigInteger.valueOf(facBase.get(j))).compareTo(BigInteger.ZERO) == 0){
+				y = y.divide(BigInteger.valueOf(facBase.get(j)));
+					
+				if (y.compareTo(BigInteger.ONE) ==0)
+					rList.add(x);
+			}
+		}
 	}
 
 	public static void main(String[] args){
 		N = new BigInteger(args[0]);
 		initFacBase();
 		L = facBase.size()+5;
+//		System.out.println(facBase.size());
 		generateRList();
-		System.out.println(rList);
+//		System.out.println(rList);
+//		System.out.println(rList.size());
 	}
 }
