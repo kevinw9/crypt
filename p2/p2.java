c8e618501d0dea7185f7a1b841a34c27478dc3b3
Kevin Wu <kevinwu1106@gmail.com>
2016-11-30 20:37:59 +0100

    finished p2

--- p2/p2.java
+++ p2/p2.java
@@ -38,17 +38,11 @@ public class p2{
 		}
 		
 		public int compNext(){
-			if ((Arrays.equals(register, initRegister)) && !zerod) {
+			if ((Arrays.equals(register, initRegister)) && !zerod)
 				return -1;
-			}
 			int sum = 0;
 			for (int i = 0; i < connectionP.length; i++)
 				sum += (connectionP[i] * register[i]);
-			/*
-			for (int i = 0; i < register.length; i++)
-				System.out.print(register[i]);
-			System.out.println();
-			*/
 			return (sum % mod);
 		}
 		
@@ -56,19 +50,20 @@ public class p2{
 
 	public static int map(int i, int j) {
 		if (i == 0)
-			return j * 2;
-		return (j * 2) + 1;
+			return j + 5;
+		return j  ;
 	}
 		
 	public static void main(String[] args){
-		LFSR mod2 = new LFSR(new int[] {1,1,0,0}, new int[] {1,0,0,0}, 2);
-		LFSR mod5 = new LFSR(new int[] {2,4,1,1}, new int[] {1,0,0,0}, 5);
+		LFSR mod2 = new LFSR(new int[] {1,1,0,0}, new int[] {0,0,0,1}, 2);
+		LFSR mod5 = new LFSR(new int[] {2,4,1,1}, new int[] {0,0,0,1}, 5);
 		int out2;
 		int out5;
-		for (int i = 0; i < 1; i++){
+		for (int i = 0; i < 10003; i++){
 			out2 = mod2.shift(mod2.compNext());
 			out5 = mod5.shift(mod5.compNext());
-			System.out.print(map(out2, out5);
+			System.out.print(map(out2, out5));
+
 		}
 	}
 }
