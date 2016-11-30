0f9a18f0a61b0acaf8c8cf55050b603bedd23827
Kevin Wu <kevinwu1106@gmail.com>
2016-11-30 19:55:45 +0100

    finished?

--- p2/p2.java
+++ p2/p2.java
@@ -10,34 +10,66 @@ public class p2{
 		private int mod;
 		private int[] connectionP;
 		private int[] register;
+		private int[] initRegister;
+		private boolean zerod;
 
-		public LFSR(int[] conP, int[] startState, int mod) {
+		public LFSR(int[] conP, int[] startState, int m) {
 			degree = conP.length;
-			mod = mod;
+			mod = m;
 			connectionP = conP;
 			register = startState; 
+			initRegister = new int[degree];
+			System.arraycopy(startState, 0, initRegister, 0, degree);
+			zerod = false;
 		}
 
 		public int shift(int next){
-			int[] temp = register;
-			for (int i = 0; i < register.length-1; i++){
-				register[i] = temp[i+1];
+			if (next == -1) {
+				zerod = true;
+				return 0;
 			}
+			zerod = false;
+			int[] temp = register;
+			int popped = register[0];
+			for (int i = 0; i < register.length-1; i++)
+				register[i] = temp[i+1];
 			register[register.length-1] = next;
-			return temp[0];
+			return popped;
 		}
-
 		
+		public int compNext(){
+			if ((Arrays.equals(register, initRegister)) && !zerod) {
+				return -1;
+			}
+			int sum = 0;
+			for (int i = 0; i < connectionP.length; i++)
+				sum += (connectionP[i] * register[i]);
+			/*
+			for (int i = 0; i < register.length; i++)
+				System.out.print(register[i]);
+			System.out.println();
+			*/
+			return (sum % mod);
+		}
+		
+	}
+
+	public static int map(int i, int j) {
+		if (i == 0)
+			return j * 2;
+		return (j * 2) + 1;
 	}
 		
 	public static void main(String[] args){
-
-		LFSR test = new LFSR(new int[] {0,0,1,1}, new int[] {1,0,0,0}, 2);
-		for (int i = 0; i < test.degree; i++)
-			System.out.println(test.register[i]);
-		test.shift(2);
-		for (int i = 0; i < test.degree; i++)
-			System.out.println(test.register[i]);
+		LFSR mod2 = new LFSR(new int[] {1,1,0,0}, new int[] {1,0,0,0}, 2);
+		LFSR mod5 = new LFSR(new int[] {2,4,1,1}, new int[] {1,0,0,0}, 5);
+		int out2;
+		int out5;
+		for (int i = 0; i < 1; i++){
+			out2 = mod2.shift(mod2.compNext());
+			out5 = mod5.shift(mod5.compNext());
+			System.out.print(map(out2, out5);
+		}
 	}
 }
 	
